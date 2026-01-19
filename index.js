df2f16577cbfd783b36a3f9c6d770af7d68798a3
nagendra <nagendrasha191@gmail.com>
2026-01-19 12:28:54 +0530

    Mongo Conneted

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require("cors");
 const connectDB = require('./config/db');
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
